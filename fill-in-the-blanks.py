3df29a3ac1e8ac164a32a0e23e032c270ad58315
xXpi314159265Xx <itd07@yahoo.com>
2016-07-06 23:10:29 +0200

    Splits part of userAnswers function into finalMessage function, created chooseAnswers functions for conditionals, reordered code

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -1,58 +1,7 @@
 # coding: utf-8
 # IPND Stage 2 Final Project
 
-# You've built a Mad-Libs game with some help from Sean.
-# Now you'll work on your own game to practice your skills and demonstrate what you've learned.
-
-# For this project, you'll be building a Fill-in-the-Blanks quiz.
-# Your quiz will prompt a user with a paragraph containing several blanks.
-# The user should then be asked to fill in each blank appropriately to complete the paragraph.
-# This can be used as a study tool to help you remember important vocabulary!
-
-# Note: Your game will have to accept user input so, like the Mad Libs generator,
-# you won't be able to run it using Sublime's `Build` feature.
-# Instead you'll need to run the program in Terminal or IDLE.
-# Refer to Work Session 5 if you need a refresher on how to do this.
-
-# To help you get started, we've provided a sample paragraph that you can use when testing your code.
-# Your game should consist of 3 or more levels, so you should add your own paragraphs as well!
-
-sample = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
-adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
-don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
-tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
-
-# The answer for ___1___ is 'function'. Can you figure out the others?
-
-# We've also given you a file called fill-in-the-blanks.pyc which is a working version of the project.
-# A .pyc file is a Python file that has been translated into "byte code".
-# This means the code will run the same as the original .py file, but when you open it
-# it won't look like Python code! But you can run it just like a regular Python file
-# to see how your code should behave.
-
-# Hint: It might help to think about how this project relates to the Mad Libs generator you built with Sean.
-# In the Mad Libs generator, you take a paragraph and replace all instances of NOUN and VERB.
-# How can you adapt that design to work with numbered blanks?
-
-# If you need help, you can sign up for a 1 on 1 coaching appointment: https://calendly.com/ipnd1-1/20min/
-
 import textwrap
-
-print "Please select a game difficulty by typing it in."
-print "Possible choices include easy, medium and hard."
-
-# Function returns difficulty of quiz selected by the user.
-def levelSelect():
-    levels = ['easy', 'medium', 'hard']
-    while True:
-        difficulty = str(raw_input()).lower()
-        if difficulty not in levels:
-            print "Please type easy, medium or hard."
-        else:
-            break
-    return difficulty
-
-difficulty = levelSelect()
 
 paragraphs = {'easy':"The ___1___ value of a number is its ___2___ from zero " 
 			"on the ___3___ line. Absolute value is always ___4___ ."
@@ -74,6 +23,17 @@ easyAnswers = ['absolute', 'distance', 'number', 'positive', 'negative']
 mediumAnswers = ['equation', 'two', 'isolate', 'sign']
 hardAnswers = ['inequality', 'inequalities', 'compound', 'and', 'or']
 
+# Function returns difficulty of quiz selected by the user.
+def levelSelect():
+    levels = ['easy', 'medium', 'hard']
+    while True:
+        difficulty = str(raw_input()).lower()
+        if difficulty not in levels:
+            print "Please type easy, medium or hard."
+        else:
+            break
+    return difficulty
+
 # Wraps printing of paragraphs
 def prettyPrint(list):
 	print ""
@@ -81,18 +41,7 @@ def prettyPrint(list):
 	for e in wrapper:
 		print e
 	print ""
-		
-prettyPrint(paragraphs[difficulty])
-
-questions = paragraphs[difficulty].split()
-
-if difficulty == "easy":
-	answerList = easyAnswers
-elif difficulty == "medium":
-	answerList = mediumAnswers
-else:
-	answerList = hardAnswers
-
+	
 def userAnswers():
 	numberOfGuesses = 5
 	answer = []
@@ -110,9 +59,6 @@ def userAnswers():
 				print "You still have " + str(numberOfGuesses) + " guesses left."
 				print ""
 				while "___" + str(blank) + "___" in questions:
-				#indices = [i for i,val in enumerate(questions) if val=="___" \
-				#+ str(blank) + "___"]
-				#print indices
 					index = questions.index("___" + str(blank) + "___")
 					questions[index] = response
 				blank += 1
@@ -123,6 +69,9 @@ def userAnswers():
 				prettyPrint(" ".join(questions))
 				print "Incorrect. You have " + str(numberOfGuesses) + \
 				" guesses left."
+	return numberOfGuesses
+	
+def finalMessage():
 	if numberOfGuesses > 0:
 		print ""
 		print "Congratulations! You filled in all of the blanks."
@@ -132,6 +81,21 @@ def userAnswers():
 		print "Sorry. Out of guesses. Try again."
 		print ""
 
+def chooseAnswers(difficulty):
+	if difficulty == "easy":
+		return easyAnswers
+	elif difficulty == "medium":
+		return mediumAnswers
+	else:
+		return hardAnswers
 
-userAnswers()
+print "Please select a game difficulty by typing it in."
+print "Possible choices include easy, medium and hard."
+difficulty = levelSelect()
+answerList = chooseAnswers(difficulty)
+prettyPrint(paragraphs[difficulty])
+questions = paragraphs[difficulty].split()
+numberOfGuesses = userAnswers()
+finalMessage()
+
 
